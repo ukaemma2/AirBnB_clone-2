b36bc32eddc7c8648665925f17dcaf48089da0d4
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-19 19:30:48 +0000

    changes

--- console.py
+++ console.py
@@ -223,6 +223,26 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
+            # for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(args).items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
+        else:
+            # for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
+                print_list.append(str(v))
+
+        print(print_list)
+
+    '''def do_all(self, args):
+        """ Shows all objects, or all objects of a class"""
+        print_list = []
+
+        if args:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
             for k, v in storage._FileStorage__objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
@@ -230,7 +250,7 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
 
-        print(print_list)
+        print(print_list)'''
 
     def help_all(self):
         """ Help information for the all command """
