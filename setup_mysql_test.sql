c30aeef51d11170e816e235322caf0ea103ba1fa
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-19 21:21:14 +0000

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,9 @@
---creates a database a new user with specified permissions
+-- Creates a databae hbnb_test_db
+-- Create a new user hbnb_test
+-- The user's password is hbnb-test_pwd
+-- The user should have all Privileges
+-- hbnb_test should have SELEC privilege on performance_scheme
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
