c570b3966472e1681751d9e90f111d9649242c7a
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-09 23:41:29 +0000

    modified

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,7 +20,7 @@ SERVER_CONFIG="server {
 	}
 
 	if (\$request_filename ~ redirect_me) {
-		rewrite ^ https://wysedotng.tech/hbnb_static permanent;
+		rewrite ^ https://emmitech.tech/hbnb_static permanent;
 	}
 
 	location = /404.html {
