b4d5edd44b1dce86bdb0e0f0fcf3d339d216ba77
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-13 10:54:43 +0000

    fixed_err

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,42 +8,29 @@ env.hosts = ["52.86.204.80", "34.202.158.153"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
-
 def do_deploy(archive_path):
-    """check and run locally"""
-
     """Fabric script that distributes
     an archive to your web server"""
 
-    if not os.path.exists(archive_path):
+    if not path.exists(archive_path):
         return False
     try:
-        tgzfile_name = archive_path.split("/")[-1]
-        fileName_not_ext = tgzfile_name.split(".")[0]
-        old_path_name = "/data/web_static/releases/{}/web_static".format(
-            fileName_not_ext)
-        new_path = '/data/web_static/releases/{}'.format(fileName_not_ext)
-        curr_path = 'data/web_static/current'
-
-        '''first run and deploy locally'''
-        run_locally = os.getenv("run_locally", None)
-        if run_locally is None:
-            api.local(f'sudo mkdir -p {new_path}')
-            api.local(f'sudo tar -zxf {archive_path} -C {new_path}')
-            api.local(f'sudo rm -rfR {curr_path}')
-            api.local(f'ln -s {new_path} {curr_path}')
-            os.environ['run_locally'] = "True"
-        print('locally deployed successfully')
-
-        '''deploy to the server remotely'''
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {new_path}')
-        run(f'tar -zxf {archive_path} -C {new_path}')
-        run(f'rm /tmp/{tagzfile_name}')
-        run(f'rm -rfR {curr_path}')
-        run(f'ln -s {new_path} {curr_path}')
-        print('remotely deployed to their server successfully')
-
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
     except Exception as e:
         return False
