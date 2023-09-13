8ec594111ddfb21a506bd6b53361c8352ca42002
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-13 10:45:32 +0000

    deployed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,68 @@
 #!/usr/bin/python3
-from fabric.api import put, run, local, env
+"""
+Fabric script to deploy tgz archive
+fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
+    -i private-key -u user
+"""
+
+from os.path import exists
 import os
-import fabric.api as api
+from fabric.api import put, run, env, local
 
-
-env.hosts = ["52.86.204.80", "34.202.158.153"]
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.hosts = ['52.86.204.80', '34.202.158.153']
 
 
 def do_deploy(archive_path):
-    """check and run locally"""
+    """
+    copies archive file from local to my webservers
+    """
 
-    """Fabric script that distributes
-    an archive to your web server"""
-
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
     try:
-        tgzfile_name = archive_path.split("/")[-1]
-        fileName_not_ext = tgzfile_name.split(".")[0]
-        old_path_name = "/data/web_static/releases/{}/web_static".format(
-            fileName_not_ext)
-        new_path = '/data/web_static/releases/{}'.format(fileName_not_ext)
-        curr_path = 'data/web_static/current'
+        file_name = archive_path.split("/")[-1].split(".")[0]
 
-        '''first run and deploy locally'''
-        run_locally = os.getenv("run_locally", None)
-        if run_locally is None:
-            api.local(f'sudo mkdir -p {new_path}')
-            api.local(f'sudo tar -zxf {archive_path} -C {new_path}')
-            api.local(f'sudo rm -rfR {curr_path}')
-            api.local(f'ln -s {new_path} {curr_path}')
-            os.environ['run_locally'] = "True"
-        print('locally deployed successfully')
+        '''run_loc = os.getenv('run_locl', None)
+        if run_loc is None:
+            
+            local("mkdir -p /data/web_static/releases/{}"
+                  .format(file_name))
 
-        '''deploy to the server remotely'''
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p {new_path}')
-        run(f'tar -zxf {archive_path} -C {new_path}')
-        run(f'rm /tmp/{tagzfile_name}')
-        run(f'rm -rfR {curr_path}')
-        run(f'ln -s {new_path} {curr_path}')
-        print('remotely deployed to their server successfully')
+            local(('mv /data/web_static/releases/{}/web_static/* ' +
+                   '/data/web_static/releases/{}/')
+                  .format(file_name, file_name))
 
+            local('rm -rf /data/web_static/releases/{}/web_static'
+                  .format(file_name))
+
+            local('rm -rf /data/web_static/current')
+
+            local(('ln -s /data/web_static/releases/{}/' +
+                   ' /data/web_static/current')
+                  .format(file_name))
+            os.environ['run_loc'] = 'True'
+        print('locally deployed successfully')'''
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
         return True
-    except Exception as e:
+    except Exception:
         return False
