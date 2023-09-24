2126c2a2d8b84084ee77f486c4f429c90dcec8a7
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-24 12:42:29 +0000

    fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
 """Starts Flask web apllication."""
 from flask import Flask
-
 app = Flask(__name__)
 '''Define the route for the root URL '/'
 '''
+
 @app.route('/', strict_slashes=False)
+
+
 def hello_HBNB():
     '''Display 'Hello HBNB! '''
     return "Hello HBNB!"
@@ -16,4 +17,4 @@ if __name__ == "__main__":
     '''Start the Flash development server
     Listen on all available network interfaces (0.0.0.0) and port 5000
     '''
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
