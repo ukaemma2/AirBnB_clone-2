a2986f49f90fcf62aef9c1e9928cd9a2f64b4ce1
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-24 13:22:34 +0000

    py_err_fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,6 +14,7 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_HBNB():
     """Display 'HBNB'"""
@@ -21,4 +22,4 @@ def hello_HBNB():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
