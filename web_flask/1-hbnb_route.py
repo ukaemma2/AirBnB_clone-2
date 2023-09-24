de6854c40f7ddb3876cd4f43b53602eef875405e
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-24 13:20:45 +0000

    removed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,10 +15,10 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
+def hello_HBNB():
     """Display 'HBNB'"""
     return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
