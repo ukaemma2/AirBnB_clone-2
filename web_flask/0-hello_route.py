51f1b10d4ece88bc6dd1f924dcee0e00c4392de5
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-24 09:54:51 +0000

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Starts Flask web apllication."""
+from flask import Flask
+
+app = Flask(__name__)
+'''Define the route for the root URL '/'
+'''
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    '''Display 'Hello HBNB! '''
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    #Start the Flash development server
+    #Listen on all available network interfaces (0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)
