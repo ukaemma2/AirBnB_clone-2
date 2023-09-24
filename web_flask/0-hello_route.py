dbfb038b8b0ab2baa11d90452721bc86e01183be
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-24 09:58:32 +0000

    fixed_err

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,9 @@ def hello_HBNB():
     '''Display 'Hello HBNB! '''
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    #Start the Flash development server
-    #Listen on all available network interfaces (0.0.0.0) and port 5000
+    '''Start the Flash development server
+    Listen on all available network interfaces (0.0.0.0) and port 5000
+    '''
     app.run(host='0.0.0.0', port=5000)
