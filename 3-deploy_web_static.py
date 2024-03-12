45008ab2d59d591d83e688a722045e5f474a931c
ukaemma2 <ukaemmanuel2000@gmail.com>
2024-03-12 14:10:31 +0000

    gg

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.196.25.60", "54.210.157.92"]
 
 
 def do_pack():
